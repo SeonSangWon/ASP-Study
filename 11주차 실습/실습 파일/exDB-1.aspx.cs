a5d2ffb46a95cfb7f0d82741415c9a3325539497
선상원 <45645824+SeonSangWon@users.noreply.github.com>
2019-06-23 17:56:56 +0900

    Add files via upload

---
+++ 11주차 실습/실습 파일/exDB-1.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace ex00
+{
+    public partial class exDB_1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=.\\SQLEXPRESS_ASP; Initial Catalog=MyDB;" +
+                "Integrated Security=true");
+
+            String sql = "Select UserID, Password, Name, Phone From member Where UserID=@UserID";
+
+            //커맨드 객체 생성(명령문만 만듬)
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.Parameters.AddWithValue("@UserID",TextBox1.Text);
+            con.Open();
+
+            //수행한 명령을 저장시킬 객체를 생성
+            //ExecuteReader() : cmd 객체를 실행
+            //연결기반 DB연동방식의 경우 SqlDataReader를 사용하며
+            //비연결기반 DB연동방식의 경우 SqlDataAdapter를 사용한다.
+            SqlDataReader rd = cmd.ExecuteReader();
+
+            //rd에서 읽어와 Label에 출력
+            while (rd.Read())
+            {
+                Label1.Text += String.Format("{0}, {1}, {2}, {3}<br />",
+                    rd["UserID"], rd["Password"], rd["Name"], rd["Phone"]);
+            }
+
+            //종료
+            rd.Close();
+            con.Close();
+        }
+    }
+}
